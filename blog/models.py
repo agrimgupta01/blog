413495799a330c69fbd5d78e0ab29221cd17d1b7
agrimgupta01 <agrim921@gmail.com>
2022-07-01 17:48:47 +0530

    Added individual blog pages and an integer field

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
 
     title = models.CharField(max_length=200)
+    author_identity = models.IntegerField(default=1)
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -9,3 +9,7 @@ from . models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
